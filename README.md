5ca76a311aa13ce46c6e624eee2a001efcbbf013
Hill Ma <mahiuchun@users.noreply.github.com>
2015-03-20 16:45:58 -0700

    update README; add variants; work around unicode issue

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Data Sources of Chinese (HK) Helper
 
 ## Cangjie
-libcangjie-table.txt is from https://github.com/Cangjians/libcangjie/blob/master/data/table.txt
+libcangjie-table.txt is from
+https://github.com/Cangjians/libcangjie/blob/master/data/table.txt
 
 ## Jyutping
-cantonese-syllables-characters.json is from https://github.com/Cangjians/libcangjie/blob/master/data/table.txt
+cantonese-syllables-characters.json is from
+https://github.com/Cangjians/libcangjie/blob/master/data/table.txt
 
 ## Mandarin
-Unihan_Readings.txt is from http://unicode.org/Public/UNIDATA/Unihan.zip
+Unihan_Readings.txt is from
+http://unicode.org/Public/UNIDATA/Unihan.zip
+
+## Simplified/Traditional
+Unihan_Variants.txt is from
+http://unicode.org/Public/UNIDATA/Unihan.zip
--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import codecs
 import json
+import sys
 
 def do_cangjie():
     chinese_to_cangjie3 = dict()
@@ -47,17 +48,58 @@ def do_mandrain():
         fields = line.split()
         if fields[1] != 'kHanyuPinyin':
             continue
-        ch = unichr(int('0x'+fields[0][2:], 16))
+        cp = int('0x'+fields[0][2:], 16)
+        if cp > sys.maxunicode:
+            continue
+        ch = unichr(cp)
         mandarin = fields[2].split(':')[1].split(',')
         chinese_to_mandarin[ch] = mandarin
     h.close()
     chinese_to_mandarin_json = json.dumps(chinese_to_mandarin, ensure_ascii=False, sort_keys=True)
     codecs.open('chinese_to_mandarin.js', 'w', encoding='utf8').write('var chinese_to_mandarin='+chinese_to_mandarin_json+'\n')
 
+def do_variants():
+    chinese_to_simplified  = dict()
+    chinese_to_traditional = dict()
+    h = codecs.open('Unihan_Variants.txt', 'r', encoding='utf8')
+    for line in h:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        fields = line.split()
+        cp = int('0x'+fields[0][2:], 16)
+        if cp > sys.maxunicode:
+            continue
+        ch = unichr(cp)
+        if fields[1] == 'kTraditionalVariant':
+            cp = int('0x'+fields[2][2:], 16)
+            if cp > sys.maxunicode:
+                continue
+            trad = unichr(cp)
+            if ch not in chinese_to_traditional:
+                chinese_to_traditional[ch] = list()
+            chinese_to_traditional[ch].append(trad)
+        elif fields[1] == 'kSimplifiedVariant':
+            cp = int('0x'+fields[2][2:], 16)
+            if cp > sys.maxunicode:
+                continue
+            simp = unichr(cp)
+            if ch not in chinese_to_simplified:
+                chinese_to_simplified[ch] = list()
+            chinese_to_simplified[ch].append(simp)
+        else:
+            continue
+    h.close()
+    chinese_to_simplified_json  = json.dumps(chinese_to_simplified,  ensure_ascii=False, sort_keys=True)
+    chinese_to_traditional_json = json.dumps(chinese_to_traditional, ensure_ascii=False, sort_keys=True)
+    codecs.open('chinese_to_simplified.js',  'w', encoding='utf8').write('var chinese_to_simplified=' +chinese_to_simplified_json +'\n')
+    codecs.open('chinese_to_traditional.js', 'w', encoding='utf8').write('var chinese_to_traditional='+chinese_to_traditional_json+'\n')
+
 def main():
     do_cangjie()
     do_jyutping()
     do_mandrain()
+    do_variants()
 
 if __name__ == '__main__':
     main()
