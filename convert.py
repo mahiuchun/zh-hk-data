d8bb1cde169fdd4d055068465d5d7881e58a0a70
Hill Ma <mahiuchun@users.noreply.github.com>
2015-03-28 16:02:25 -0700

    merge cangjie 3/5 in order to reduce file size

--- convert.py
+++ convert.py
@@ -3,8 +3,7 @@ import json
 import sys
 
 def do_cangjie():
-    chinese_to_cangjie3 = dict()
-    chinese_to_cangjie5 = dict()
+    chinese_to_cangjie = dict()
     h = codecs.open('libcangjie-table.txt', 'r', encoding='utf8')
     for line in h:
         line = line.strip()
@@ -19,19 +18,20 @@ def do_cangjie():
         cj5 = fields[12].split(',')
         if not is_chinese:
             continue
-        if cj3[0] != 'NA':
-            chinese_to_cangjie3[ch] = list()
-            for cj in cj3:
-                chinese_to_cangjie3[ch].append(cj)
+        chinese_to_cangjie[ch] = list()
         if cj5[0] != 'NA':
-            chinese_to_cangjie5[ch] = list()
             for cj in cj5:
-                chinese_to_cangjie5[ch].append(cj)
+                chinese_to_cangjie[ch].append(cj)
+        if cj3[0] != 'NA':
+            for cj in cj3:
+                if cj in chinese_to_cangjie[ch]:
+                    continue
+                chinese_to_cangjie[ch].append(cj)
+        if len(chinese_to_cangjie[ch]) == 0:
+            del chinese_to_cangjie[ch]
     h.close()
-    chinese_to_cangjie3_json = json.dumps(chinese_to_cangjie3, ensure_ascii=False, sort_keys=True)
-    chinese_to_cangjie5_json = json.dumps(chinese_to_cangjie5, ensure_ascii=False, sort_keys=True)
-    codecs.open('chinese_to_cangjie3.js', 'w', encoding='utf8').write('var chinese_to_cangjie3='+chinese_to_cangjie3_json+'\n')
-    codecs.open('chinese_to_cangjie5.js', 'w', encoding='utf8').write('var chinese_to_cangjie5='+chinese_to_cangjie5_json+'\n')
+    chinese_to_cangjie_json = json.dumps(chinese_to_cangjie, ensure_ascii=False, sort_keys=True)
+    codecs.open('chinese_to_cangjie.js', 'w', encoding='utf8').write('var chinese_to_cangjie='+chinese_to_cangjie_json+'\n')
 
 def do_jyutping():
     chinese_to_jyutping = json.loads(codecs.open('cantonese-syllables-characters.json', encoding='utf8').read())
